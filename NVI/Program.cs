44f7aa7ec1be3383b58b627b4398d860ff9a0ced
martinprogrammer <martinprogrammer>
2014-11-01 18:05:14 +0000

    Non Virtual Interface Pattern Example
    Separates functionality of the base class into Core and non core.  The
    core functionality are non-virtual - but the non public methods that the
    core functionality is calling on are virtual, so can be overriden by the
    derived.

---
+++ NVI/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base myBase = new Base();
+            myBase.MyProperty = 2;
+            Console.WriteLine(myBase.MyProperty);
+            Derived myDerived = new Derived();
+            myDerived.MyProperty = 17;
+            Console.WriteLine(myDerived.MyProperty);
+            Console.Read();
+         
+        }
+    }
+
+    public class Base
+    {
+        public virtual int MyProperty { get; set; }
+        public virtual void DoWork()
+        {
+            CoreDoWork();
+        }
+
+        protected virtual void CoreDoWork()
+        {
+            Console.WriteLine("Base.DoWork()");
+            Console.Read();
+        }
+    }
+
+    public class Derived : Base
+    {
+        public override int MyProperty { get { return 1; } }
+
+         public override void DoWork()
+        {
+            Console.WriteLine(hello);
+            Console.Read();
+        }
+        protected override void CoreDoWork()
+        {
+            Console.WriteLine("Derived one");
+            base.CoreDoWork();
+            Console.Read();
+            
+        }
+
+    }
+}
